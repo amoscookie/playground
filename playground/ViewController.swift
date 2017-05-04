e041c21b97a027da6d761b37c65ebca184181a2f
Amos Seah <amos_seah1981@yahoo.com.sg>
2017-05-04 23:20:51 +0800

    hello world

---
+++ playground/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by Amos Seah on 4/5/17.
+//  Copyright © 2017 Amos Seah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
